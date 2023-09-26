3c871eea959319d418a329d8f6a3f10ae900fa51
Eduwer Camacaro <eduwerc@gmail.com>
2023-09-26 15:48:43 -0500

    TODO

--- streams/src/main/java/org/apache/kafka/streams/processor/internals/StoreChangelogReader.java
+++ streams/src/main/java/org/apache/kafka/streams/processor/internals/StoreChangelogReader.java
@@ -679,6 +679,7 @@ public class StoreChangelogReader implements ChangelogReader {
                     throw new StreamsException("State restore listener failed on batch restored", e);
                 }
             } else {
+                //TODO eduwer proper values
                 standbyTaskUpdateListener.onBatchRestored(partition, storeName, 0L, 0L, 0L);
             }
         }
