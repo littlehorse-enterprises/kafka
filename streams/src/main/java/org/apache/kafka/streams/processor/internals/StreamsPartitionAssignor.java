f616fed06cdc8e90d64447224a17928b9f5b25c6
Colt McNealy <colt@littlehorse.io>
2025-11-04 21:14:52 -0800

    fix(streams): avoid fencing during rebalance
    - Commit all committable tasks in the start of the onAssignment() method, which can help prevent transaction timeouts when blocking on the State Updater to release tasks
    - Increase the flush threshold for tasks in the state updater to 1M records (not 10k) in order to reduce thrashing of rocksdb in background I/O.

--- streams/src/main/java/org/apache/kafka/streams/processor/internals/StreamsPartitionAssignor.java
+++ streams/src/main/java/org/apache/kafka/streams/processor/internals/StreamsPartitionAssignor.java
@@ -1458,6 +1458,14 @@ public class StreamsPartitionAssignor implements ConsumerPartitionAssignor, Conf
 
     @Override
     public void onAssignment(final Assignment assignment, final ConsumerGroupMetadata metadata) {
+        taskManager.commit(
+            taskManager.allOwnedTasks()
+                .values()
+                .stream()
+                .filter(t -> t.commitNeeded())
+                .collect(Collectors.toSet())
+        );
+
         final List<TopicPartition> partitions = new ArrayList<>(assignment.partitions());
         partitions.sort(PARTITION_COMPARATOR);
 
